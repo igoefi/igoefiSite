95c126ddea7b27187d939665c379fe3691279730
igoefi <igoefi@mail.ru>
2024-04-29 23:35:05 +0300

    AddDjangoApplication

---
+++ mainYesSite/product/models.py
@@ -0,0 +1,29 @@
+from io import BytesIO
+from PIL import Image
+
+from django.core.files import File
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+
+    class Meta:
+        ordering = ('name',)
+
+    def ___str___(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f"/{self.slug}/"
+    
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name="products", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    thrumbnail = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
